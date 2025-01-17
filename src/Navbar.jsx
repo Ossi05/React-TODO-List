368f831475bb14be7bd3ecba2006b43514e01c2b
Ossi <ossijuhani.ikonen@gmail.com>
2025-01-17 16:59:28 +0200

    Linkki lisätty Githubiin

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,6 +2,8 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { IconButton, Link } from '@mui/material';
 export default function Navbar({ title }) {
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -10,15 +12,17 @@ export default function Navbar({ title }) {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         {title}
                     </Typography>
-                    {/* <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <AddIcon />
-                    </IconButton> */}
+                    <Link href="https://github.com/Ossi05/React-TODO-List" rel="noreferrer noopener" target="_blank" color="inherit">
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <GitHubIcon />
+                        </IconButton>
+                    </Link>
                 </Toolbar>
             </AppBar>
         </Box>
